c6d3387e958913eb0f2c73e469cc49912e6618dd
timur <stukantimur811@gmail.com>
2024-07-19 11:39:37 +0300

    viewsets class pattern

--- station/views.py
+++ station/views.py
@@ -1,14 +1,9 @@
-from rest_framework import generics
+from rest_framework import viewsets
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
 
-class BusList(generics.ListCreateAPIView):
-    queryset = Bus.objects.all()
-    serializer_class = BusSerializer
-
-
-class BusDetail(generics.RetrieveUpdateDestroyAPIView):
+class BusViewSet(viewsets.ModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
