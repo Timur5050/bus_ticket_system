dbd4df7a6db7d7828a92a7f1095ae527fc6ab2cc
timur <stukantimur811@gmail.com>
2024-07-19 11:40:11 +0300

    endpoint for viewsets class pattern

--- station/urls.py
+++ station/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from station.views import BusList, BusDetail
+from station.views import BusViewSet
 
+bus_list = BusViewSet.as_view(actions={"get": "list", "post": "create"})
+
+bus_detail = BusViewSet.as_view(actions={
+            "get": "retrieve",
+            "put": "update",
+            "patch": "partial_update",
+            "delete": "destroy"
+        })
 
 urlpatterns = [
-    path("buses/", BusList.as_view(), name="bus_list"),
-    path("buses/<int:pk>/", BusDetail.as_view(), name="bus_detail"),
+    path("buses/", bus_list, name="bus_list"),
+    path("buses/<int:pk>/", bus_detail, name="bus_detail"),
 ]
 app_name = "station"
