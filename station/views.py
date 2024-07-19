e774c26a8a4e31e082b8b311b671d590cc1d32a6
timur <stukantimur811@gmail.com>
2024-07-19 11:17:24 +0300

    generic view class

--- station/views.py
+++ station/views.py
@@ -1,42 +1,14 @@
-from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework import status, generics, mixins
-from rest_framework.views import APIView
+from rest_framework import generics
 
 from station.models import Bus
 from station.serializers import BusSerializer
 
 
-class BusList(
-    generics.GenericAPIView,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-):
+class BusList(generics.ListCreateAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class BusDetail(
-    generics.GenericAPIView,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-):
+class BusDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
-
-    def get(self, request, *args, **kwags):
-        return self.retrieve(request, *args, **kwags)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
