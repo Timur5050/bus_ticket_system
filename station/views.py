9c9908f246404c824365234d1872d451ed143791
timur <stukantimur811@gmail.com>
2024-07-21 14:10:19 +0300

    created veiw for related models

--- station/views.py
+++ station/views.py
@@ -1,9 +1,50 @@
 from rest_framework import viewsets
 
-from station.models import Bus
-from station.serializers import BusSerializer
+from station.models import Bus, Trip, Facility
+from station.serializers import (
+    BusSerializer,
+    TripSerializer,
+    TripListSerializer,
+    BusListSerializer,
+    FacilitySerializer,
+    BusRetrieveSerializer, TripRetrieveSerializer
+)
+
+
+class FacilityViewSet(viewsets.ModelViewSet):
+    queryset = Facility.objects.all()
+    serializer_class = FacilitySerializer
 
 
 class BusViewSet(viewsets.ModelViewSet):
     queryset = Bus.objects.all()
-    serializer_class = BusSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return BusListSerializer
+        elif self.action == "retrieve":
+            return BusRetrieveSerializer
+        return BusSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ('list', 'retrieve'):
+            return queryset.prefetch_related("facilities")
+        return queryset
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TripListSerializer
+        elif self.action == "retrieve":
+            return TripRetrieveSerializer
+        return TripSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action in ('list', 'retrieve'):
+            return queryset.select_related("bus")
+        return queryset
